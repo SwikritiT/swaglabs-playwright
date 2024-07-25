ce97b4d94ae47776f528072cc4180cf88d7dfde3
Swikriti Tripathi <swikriti808@gmail.com>
2024-07-25 22:44:50 +0545

    make headless defalut
    Signed-off-by: Swikriti Tripathi <swikriti808@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig, devices } from "@playwright/test"
 
-const workers = process.env.WORKERS ? parseInt(process.env.WORKERS, 10) : undefined;
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const workers = process.env.WORKERS
+	? parseInt(process.env.WORKERS, 10)
+	: undefined
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -33,55 +28,18 @@ export default defineConfig({
 		trace: "on-first-retry",
 	},
 
-	/* Configure projects for major browsers */
+	/* Configure projects for major browsers, currently only set for chromium */
 	projects: [
 		{
 			name: "chromium",
 			use: {
 				...devices["Desktop Chrome"],
-				headless: process.env.PLAYWRIGHT_HEADLESS === "true", // Set headless based on the environment variable
+				headless: process.env.PLAYWRIGHT_HEADLESS !== "false", // Set headless based on the environment variable
 				launchOptions: {
 					slowMo: parseInt(process.env.SLOW_MO ?? "0", 10) || 0,
 					timeout: parseInt(process.env.TIMEOUT ?? "60000", 10) || 60000,
 				},
 			},
 		},
-
-		// {
-		//   name: 'firefox',
-		//   use: { ...devices['Desktop Firefox'] },
-		// },
-
-		// {
-		//   name: 'webkit',
-		//   use: { ...devices['Desktop Safari'] },
-		// },
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
 	],
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
 })
